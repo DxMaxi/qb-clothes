c50c1ac87c0e66b418464ed37a741b43acb3f007
Enzo <86290612+Enzo-DC@users.noreply.github.com>
2022-01-17 19:11:31 +0100

    fix spawn ped

--- qb-clothes/client/ped.lua
+++ qb-clothes/client/ped.lua
@@ -1,3 +1,30 @@
+local function loadAnimDict(dict)
+    while (not HasAnimDictLoaded(dict)) do
+        RequestAnimDict(dict)
+        Wait(5)
+    end
+end
+
+RegisterNetEvent('qube-creaped:client',function(pedd, coords, heading, modello, dict, anim)
+	if not HasModelLoaded(modello) then
+		RequestModel(modello)
+		Wait(10)
+	end
+	while not HasModelLoaded(modello) do 
+		Wait(10)
+	end
+	pedd = CreatePed(1, modello, coords, heading, false, false)
+	FreezeEntityPosition(pedd, true)
+	SetPedConfigFlag(pedd, 2, true)
+	SetPedConfigFlag(pedd, 17, true)
+	SetEntityInvincible(pedd, true)
+    SetBlockingOfNonTemporaryEvents(pedd, true)
+	loadAnimDict(dict) 
+	while not TaskPlayAnim(pedd, dict, anim, 8.0, 1.0, -1, 17, 0, 0, 0, 0) do
+		Wait(1000)
+	end
+end)
+
 CreateThread(function()
 	Wait(1000)
 	for k, v in pairs(Config.PedQube) do
